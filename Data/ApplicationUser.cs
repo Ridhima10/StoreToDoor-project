3d274972f5639a4d2eb4253d161a17ccfdf362bc
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-03-07 17:42:08 +0530

    add more fields to Application User

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -7,5 +7,13 @@ namespace StoreToDoor.Data
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public int? pin { get; set; }
     }
 }
---
+++ Data/Migrations/20220307120721_AddMissingUserFields.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreToDoor.Data.Migrations
+{
+    public partial class AddMissingUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "pin",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "pin",
+                table: "AspNetUsers");
+        }
+    }
+}
